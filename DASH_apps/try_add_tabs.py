c481c80230e3c8f415d465aab74eb3dafc23d025
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-04-27 09:42:32 +0200

    Create server separetely to allow integration with gunicorn

--- DASH_apps/try_add_tabs.py
+++ DASH_apps/try_add_tabs.py
@@ -42,8 +42,11 @@ dfi_swe.insert(loc=1, column="Region", value="Sweden")
 
 df1_intense = pd.concat([df1_intense, dfi_swe])
 
+server = flask.Flask(__name__)
+
 app = dash.Dash(
     __name__,
+    server=server,
     suppress_callback_exceptions=True,
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
