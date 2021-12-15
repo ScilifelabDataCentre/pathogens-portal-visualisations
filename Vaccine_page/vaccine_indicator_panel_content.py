9c6fe2665ddf8082483b7aa8d3497b5607122b91
LianeHughes <68756672+LianeHughes@users.noreply.github.com>
2021-12-15 06:58:18 +0100

    separate live text from methods barchart

--- Vaccine_page/vaccine_indicator_panel_content.py
+++ Vaccine_page/vaccine_indicator_panel_content.py
@@ -1,4 +1,4 @@
-# Will create indicators and bar chart to display on page (can break script down if components are needed separately)
+# Will create indicators to display on page
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
@@ -217,76 +217,3 @@ data_dictionary = {
 
 with open("live_text_inserts.json", "w") as outfile:
     json.dump(data_dictionary, outfile)
-
-# Now will make a dataframe so that we can create a grouped bar chart as a summary
-
-vaccine_dose_totals = pd.DataFrame()
-vaccine_dose_totals["Doses"] = ["1", "2", "3"]
-vaccine_dose_totals["sixteens_perc"] = [
-    one_dose_swe,
-    least_two_dose_swe,
-    third_vacc_dose_tot,
-]
-vaccine_dose_totals["POP_perc"] = [
-    one_dose_pop,
-    least_two_dose_pop,
-    third_vacc_dose_pop,
-]
-
-# initiate barchart
-
-trace1 = go.Bar(
-    x=vaccine_dose_totals["Doses"],
-    y=vaccine_dose_totals["sixteens_perc"],
-    name="Method including 16+",
-    marker_color="rgb(5,48,97)",
-    hovertemplate="Number of Doses: %{x}" + "<br>Percent Receiving the Dose: %{y:.2f}%",
-)
-trace2 = go.Bar(
-    x=vaccine_dose_totals["Doses"],
-    y=vaccine_dose_totals["POP_perc"],
-    name="Whole Population Method",
-    marker_color="rgb(178,24,43)",
-    # marker_pattern_shape="/",
-    hovertemplate="Number of Doses: %{x}" + "<br>Percent Receiving the Dose: %{y:.2f}%",
-)
-
-# figure layout
-fig = go.Figure(data=[trace1, trace2])
-fig.update_layout(
-    plot_bgcolor="white",
-    font=dict(size=16),
-    margin=dict(l=0, r=50, t=0, b=0),
-    showlegend=True,
-    legend=dict(
-        title=" ",
-        orientation="h",
-        # yanchor="bottom",
-        y=1.2,
-        # xanchor="right",
-        # x=0.5,
-        font=dict(size=16),
-    ),
-)
-# modify x-axis
-fig.update_xaxes(
-    title="<b>Number of Doses Received</b>",
-    showgrid=True,
-    linecolor="black",
-)
-# modify y-axis
-fig.update_yaxes(
-    title="<b>Percentage Vaccinated</b>",
-    showgrid=True,
-    gridcolor="lightgrey",
-    linecolor="black",
-    # change range to envelope the appropriate range
-    range=[0, 100],
-)
-
-# fig.show()
-
-if not os.path.isdir("Plots/"):
-    os.mkdir("Plots/")
-
-fig.write_json("Plots/Total_vaccinated_barchart.json")
