7ef94fe5fdf296b0b6656ce28b6d677547677384
LianeHughes <liane.hughes@scilifelab.uu.se>
2022-03-23 10:25:00 +0100

    Update combined_stockholm_logyaxis.py
    from 'Relative copy number of<br>SARS-CoV-2 to PMMoV' to 'N3-gene copy number per<br>PMMoV gene copy number'

--- wastewater/combined_stockholm_logyaxis.py
+++ wastewater/combined_stockholm_logyaxis.py
@@ -121,7 +121,7 @@ fig.update_xaxes(
     tickangle=45,
 )
 fig.update_yaxes(
-    title="<b>Relative copy number of<br>SARS-CoV-2 to PMMoV, log</b>",
+    title="<b>N3-gene copy number per<br>PMMoV gene copy number, log</b>",
     showgrid=True,
     gridcolor="lightgrey",
     linecolor="black",
