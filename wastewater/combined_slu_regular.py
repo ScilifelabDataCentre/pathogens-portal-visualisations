bc6f1c04fea23b3431736e217890e532aa5e9239
LianeHughes <liane.hughes@scilifelab.uu.se>
2022-04-08 12:41:43 +0200

    modify y-axis title
    change to N1-gene copy number per PMMOV<br>gene copy number x 10^4

--- wastewater/combined_slu_regular.py
+++ wastewater/combined_slu_regular.py
@@ -166,7 +166,7 @@ fig.update_xaxes(
     tickangle=45,
 )
 fig.update_yaxes(
-    title="<b>Relative copy number of<br>SARS-CoV-2 to PMMoV (%)</b>",
+    title="<b>N1-gene copy number per PMMOV<br>gene copy number x 10^4</b>",
     showgrid=True,
     gridcolor="lightgrey",
     linecolor="black",
