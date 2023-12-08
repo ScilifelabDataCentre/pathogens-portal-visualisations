8ce401f7fcbc4877512c061eb18c665bae683e10
LianeHughes <68756672+LianeHughes@users.noreply.github.com>
2023-12-08 19:30:58 +0100

    updated methods graphs

---
+++ wastewater/enteric_viruses_gu_update.py
@@ -0,0 +1,306 @@
+import os
+import pandas as pd
+import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime as dt
+from plotly.io import write_image
+
+# # Knivsta, Vaxholm and Österåker.
+# Göteborg, Malmö and Stockholm-Käppala
+
+#  Method to get proper range for y axis
+def get_yaxis_range(data):
+    nz_data = data[data != 0]
+    return [0, max((nz_data.mean() * 10), max(nz_data)) * 1.15]
+
+
+wastewater_data = pd.read_excel(
+    "data/wastewater_data_gu_allviruses_2023.xlsx",
+    sheet_name="all_viruses",
+    header=0,
+    engine="openpyxl",
+)
+# Convert the dates
+wastewater_data["year"] = (wastewater_data["week"].str[:4]).astype(int)
+wastewater_data["week_no"] = wastewater_data["week"].str[-3:]
+wastewater_data["week_no"] = (
+    wastewater_data["week_no"].str.replace("-", "", regex=True)
+).astype(int)
+# set the date to the start of the week (Monday)
+wastewater_data["day"] = 1
+wastewater_data["date"] = wastewater_data.apply(
+    lambda row: dt.fromisocalendar(row["year"], row["week_no"], row["day"]), axis=1
+)
+wastewater_data = wastewater_data[(wastewater_data["date"] >= "2023-01-09")]
+
+fig = go.Figure(
+    data=[
+        go.Scatter(
+            name="Enterovirus",
+            x=wastewater_data.date,
+            y=wastewater_data.enterovirus,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[0], size=7),
+            marker_symbol="square",
+            line=dict(color=px.colors.diverging.RdBu[0], width=2),
+            visible=True,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+        go.Scatter(
+            name="PMMoV",
+            x=wastewater_data.date,
+            y=wastewater_data.PMMoV,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[1], size=7),
+            marker_symbol="cross",
+            line=dict(color=px.colors.diverging.RdBu[1], width=2),
+            visible=False,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+        go.Scatter(
+            name="Adenovirus",
+            x=wastewater_data.date,
+            y=wastewater_data.adenovirus,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[2], size=7),
+            marker_symbol="hourglass",
+            line=dict(color=px.colors.diverging.RdBu[2], width=2),
+            visible=False,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+        go.Scatter(
+            name="GG2",
+            x=wastewater_data.date,
+            y=wastewater_data.GG2,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[3], size=7),
+            marker_symbol="cross",
+            line=dict(color=px.colors.diverging.RdBu[3], width=2),
+            visible=False,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+        go.Scatter(
+            name="Astrovirus",
+            x=wastewater_data.date,
+            y=wastewater_data.astrovirus,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[8], size=7),
+            marker_symbol="square",
+            line=dict(color=px.colors.diverging.RdBu[8], width=2),
+            visible=False,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+        go.Scatter(
+            name="Sapovirus",
+            x=wastewater_data.date,
+            y=wastewater_data.sapovirus,
+            mode="lines+markers",
+            marker=dict(color=px.colors.diverging.RdBu[9], size=7),
+            marker_symbol="cross",
+            line=dict(color=px.colors.diverging.RdBu[9], width=2),
+            visible=False,
+            customdata=wastewater_data,
+            hovertemplate="Date (week commencing): %{x}<br>Week: %{customdata[0]}<br>Relative amount of virus: %{y}<extra></extra>",
+        ),
+    ]
+)
+
+# Update layouts
+fig.update_layout(
+    plot_bgcolor="white",
+    autosize=True,
+    font=dict(size=14),
+    margin=dict(r=150, t=65, b=0, l=0),
+    # width=900,
+    # height=500,
+    legend=dict(yanchor="top", y=0.95, xanchor="left", x=0.99, font=dict(size=14)),
+    # hovermode="x unified",
+    hoverdistance=1,
+)
+fig.update_xaxes(
+    title="<br><b>Date (Week Commencing)</b>",
+    showgrid=True,
+    linecolor="black",
+    tickangle=45,
+)
+fig.update_yaxes(
+    title="<b>Number of viral genome copies per day<br></b>",
+    showgrid=True,
+    gridcolor="lightgrey",
+    linecolor="black",
+    # below ensures a zeroline on Y axis. Made it black to be clear it's different from other lines
+    rangemode="tozero",
+    # Using enterovirus for range as it is the first trace in that graph
+    range=[
+        0,
+        max(
+            (wastewater_data["enterovirus"].mean() * 10),
+            max(wastewater_data.enterovirus),
+        )
+        * 1.15,
+    ],
+)
+
+# select viruses
+
+button_layer_1_height = 1.0
+fig.update_layout(
+    updatemenus=[
+        dict(
+            buttons=list(
+                [
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    True,
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                ]
+                            },
+                            {
+                                "yaxis.range": get_yaxis_range(
+                                    wastewater_data.enterovirus
+                                )
+                            },
+                        ],
+                        label="Enterovirus",
+                    ),
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    False,
+                                    False,
+                                    True,
+                                    False,
+                                    False,
+                                    False,
+                                ]
+                            },
+                            {
+                                "yaxis.range": get_yaxis_range(
+                                    wastewater_data.adenovirus
+                                )
+                            },
+                        ],
+                        label="Adenovirus",
+                    ),
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    False,
+                                    False,
+                                    False,
+                                    True,
+                                    False,
+                                    False,
+                                ]
+                            },
+                            {"yaxis.range": get_yaxis_range(wastewater_data.GG2)},
+                        ],
+                        label="GG2",
+                    ),
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                    True,
+                                    False,
+                                ]
+                            },
+                            {
+                                "yaxis.range": get_yaxis_range(
+                                    wastewater_data.astrovirus
+                                )
+                            },
+                        ],
+                        label="Astrovirus",
+                    ),
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                    True,
+                                ]
+                            },
+                            {"yaxis.range": get_yaxis_range(wastewater_data.sapovirus)},
+                        ],
+                        label="Sapovirus",
+                    ),
+                    dict(
+                        method="update",
+                        args=[
+                            {
+                                "visible": [
+                                    False,
+                                    True,
+                                    False,
+                                    False,
+                                    False,
+                                    False,
+                                ]
+                            },
+                            {"yaxis.range": get_yaxis_range(wastewater_data.PMMoV)},
+                        ],
+                        label="PMMoV",
+                    ),
+                ]
+            ),
+            type="buttons",
+            # direction="right",
+            pad={"r": 10, "t": 10},
+            showactive=True,
+            x=1.07,
+            xanchor="left",
+            y=button_layer_1_height,
+            yanchor="top",
+        ),
+    ]
+)
+
+# fig.update_layout(
+#     annotations=[
+#         dict(
+#             text="Virus:",
+#             x=1.1,
+#             xref="paper",
+#             y=1.05,
+#             yref="paper",
+#             showarrow=False,
+#         ),
+#     ]
+# )
+
+# Below can show figure locally in tests
+# fig.show()
+
+if not os.path.isdir("Plots/"):
+    os.mkdir("Plots/")
+fig.write_json("Plots/enteric_graph_gu_new.json")
+
+# print(fig.to_json())
---
+++ wastewater/gothenburg_covid_update.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime as dt
+from datetime import date, timedelta
+from plotly.io import write_image
+
+wastewater_data = pd.read_excel(
+    "data/wastewater_data_gu_allviruses_2023.xlsx",
+    sheet_name="all_viruses",
+    header=0,
+    engine="openpyxl",
+    keep_default_na=False,
+)
+# wastewater_data = pd.read_csv(
+#     "https://blobserver.dc.scilifelab.se/blob/wastewater_data_gu.csv",
+#     sep=";",
+# )
+
+wastewater_data["year"] = (wastewater_data["week"].str[:4]).astype(int)
+wastewater_data["week_no"] = wastewater_data["week"].str[-3:]
+wastewater_data["week_no"] = (
+    wastewater_data["week_no"].str.replace("-", "", regex=True)
+).astype(int)
+# set the date to the start of the week (Monday)
+wastewater_data["day"] = 1
+wastewater_data["date"] = wastewater_data.apply(
+    lambda row: dt.fromisocalendar(row["year"], row["week_no"], row["day"]), axis=1
+)
+
+# colours for plot
+colours = px.colors.diverging.RdBu
+
+wastewater_data.rename(columns={"SARS-CoV-2": "covid"}, inplace=True)
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Bar(
+        name="Relative amount of SARS-CoV-2",
+        x=wastewater_data.date,
+        y=wastewater_data.covid,
+        marker_color=px.colors.diverging.RdBu[1],
+        customdata=wastewater_data,
+        hovertemplate="Week: %{customdata[0]}<br>Relative amount of SARS-CoV-2: %{y}<extra></extra>",
+    )
+)
+
+fig.update_layout(
+    plot_bgcolor="white",
+    autosize=True,
+    font=dict(size=12),
+    margin=dict(r=0, t=0, b=0, l=0),  # changed from 65 when buttons remove
+    legend=dict(yanchor="top", y=0.95, xanchor="left", x=0.99, font=dict(size=16)),
+    hovermode="x unified",
+    hoverdistance=1,
+)
+fig.update_xaxes(
+    title="<br><b>Date (Week Commencing)</b>",
+    showgrid=True,
+    linecolor="black",
+    tickangle=45,
+    # rangebreaks=[dict(values=dd)],
+)
+fig.update_yaxes(
+    title="<b>Number of SARS-CoV-2 viral genome copies per day</b>",
+    showgrid=True,
+    gridcolor="lightgrey",
+    linecolor="black",
+    # below ensures a zeroline on Y axis. Made it black to be clear it's different from other lines
+    zeroline=True,
+    zerolinecolor="black",
+    # below ensures a zeroline on Y axis. Made it black to be clear it's different from other lines
+    rangemode="tozero",
+    # Below sets the range to be at least 10 times
+    range=[
+        0,
+        max((wastewater_data["covid"].mean()) * 10, max(wastewater_data.covid)) * 1.15,
+    ],
+)
+
+# fig.add_vrect(
+#     x0="2022-11-10",
+#     x1="2023-01-06",
+#     # annotation_text="Data missing",
+#     # annotation_position="top left",
+#     fillcolor=px.colors.diverging.RdBu[10],
+#     opacity=0.5,
+#     line_width=0,
+# )
+# fig.add_vline(
+#     x=datetime.datetime.strptime("2023-01-11", "%Y-%m-%d").timestamp() * 1000,
+#     # annotation_text=" Break in data collection",
+#     #    annotation_position="top left",
+#     fillcolor=px.colors.diverging.RdBu[10],
+#     opacity=1,
+#     line_width=5,
+#     line_dash="dash",
+# )
+
+# fig.update_layout(
+#     updatemenus=[
+#         dict(
+#             type="buttons",
+#             direction="right",
+#             active=0,
+#             x=1.1,
+#             y=1.1,
+#             xanchor="right",
+#             yanchor="top",
+#             buttons=list(
+#                 [
+#                     dict(
+#                         label="Reselect all areas",
+#                         method="update",
+#                         args=[
+#                             {"visible": [True]},
+#                         ],
+#                     ),
+#                     dict(
+#                         label="Deselect all areas",
+#                         method="update",
+#                         args=[
+#                             {"visible": "legendonly"},
+#                         ],
+#                     ),
+#                 ]
+#             ),
+#         )
+#     ]
+# )
+# Below can show figure locally in tests
+# fig.show()
+
+# Prints as a json file
+fig.write_json("wastewater_gothenburg_new.json")
+
+# fig.write_image("wastewater_gothenburg_line.png")
+
+# print(fig.to_json())
