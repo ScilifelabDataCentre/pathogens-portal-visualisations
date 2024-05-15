fdacbfa950953dfedfabb3784404184f1c4482dc
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2024-05-15 10:47:22 +0200

    Add annotation text box on cumulative graph
    This annotation shows the total sum of tests and the proportion of
    positive tests
    
    Co-authored-by: LianeHughes <liane.hughes@scilifelab.uu.se>

--- serology/cumulative-serology-tests.py
+++ serology/cumulative-serology-tests.py
@@ -33,17 +33,12 @@ fig = px.line(
 )
 
 
-# Set the colours for each class
-fig.update_traces(
-    marker_color="white",
-    marker_line_width=3,
-    marker_size=8,
-)
-
-
 # Set figure traces
 fig.update_traces(
     hovertemplate=None,
+    marker_color="white",
+    marker_line_width=2,
+    marker_size=8,
 )
 
 
@@ -51,7 +46,7 @@ fig.update_traces(
 fig.update_layout(
     plot_bgcolor="white",
     autosize=True,
-    margin=dict(r=0, t=0, b=0, l=0),
+    margin=dict(r=180, t=0, b=0, l=0),
     hovermode="x unified",
     legend_traceorder="reversed",
 )
@@ -72,6 +67,44 @@ fig.update_yaxes(
     gridcolor="lightgrey",
     zeroline=True,
     zerolinecolor="black",
+)
+
+
+# Add an annotation to display the total number of tests and the proportion of positive tests
+
+# Calculate the total sum
+sum_total = serology_data["count"].sum()
+
+# Calculate the proportion of positive tests
+sum_positive_negative = (
+    serology_data[serology_data["class"] == "positive"]["count"].sum()
+    + serology_data[serology_data["class"] == "negative"]["count"].sum()
+)
+
+proportion_positive = 100 * (
+    serology_data[serology_data["class"] == "positive"]["count"].sum()
+    / sum_positive_negative
+)
+
+# Format accordingly
+proportion_positive = f"{proportion_positive:.2f}%"
+
+
+# Add figure annotation
+fig.add_annotation(
+    text=f"<b>Sum total</b><br>{sum_total}<br><br><b>Proportion<br>positive</b><br>{proportion_positive}",
+    showarrow=False,
+    xref="paper",
+    yref="paper",
+    xanchor="left",
+    x=1.02,
+    y=0.585,
+    bgcolor="#FFFFFF",
+    bordercolor="black",
+    borderwidth=2,
+    borderpad=10,
+    font_color="black",
+    font_size=14,
 )
 
 
